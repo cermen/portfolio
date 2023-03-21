26a6e4b09026eea7bd85b0751aa8fd1fbe144d2d
jyu <yjs01016@naver.com>
2023-03-21 13:26:30 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -10,4 +10,36 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
-}); 
+});
+
+// const home = document.querySelector('#home');
+// const about = document.querySelector('#about');
+// const skills = document.querySelector('#skills');
+// const work = document.querySelector('#work');
+// const testimonials = document.querySelector('#testimonials');
+// const contact = document.querySelector('#contact');
+
+// const sections = [ home, about, skills, work, testimonials, contact ];
+
+// const buttons = document.querySelectorAll('.navbar__menu__item');
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', () => {
+//         sections[i].scrollIntoView();
+//     });
+// }
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link === null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link)
+    scrollTo.scrollIntoView({behavior: "smooth"});
+})
+
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener('click', () => {
+    contact.scrollIntoView();
+});
